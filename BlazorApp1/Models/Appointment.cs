4036b7bbdce4d220edc807c27cd21175f82894b1
JMazurekCodeArt <jmazurek.artcode@gmail.com>
2024-05-12 12:01:55 +0200

    Dodaj pliki projektów.

---
+++ BlazorApp1/Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorApp1.Models
+{
+    public class Appointment
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
